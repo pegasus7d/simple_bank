a9de698b326e691a345c450ff76c2002e40b9ba6
debayan-oneLot <i.debayan@one-lot.com>
2023-12-23 20:03:18 +0530

    added environment variables using viper

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/pegasus7d/simplebank/api"
 	db "github.com/pegasus7d/simplebank/db/sqlc"
+	"github.com/pegasus7d/simplebank/db/util"
 )
 
 const (
@@ -18,7 +19,11 @@ const (
 
 
 func main(){
-	conn,err:=sql.Open(dbDriver,dbSource)
+	config,err:=util.LoadConfig(".")
+	if err!=nil{
+		log.Fatal("Cannot load config files",err)
+	}
+	conn,err:=sql.Open(config.DBDriver,config.DBSource)
 	if err !=nil{
 		log.Fatal("Cannot connect to db:",err)
 	}
@@ -27,7 +32,7 @@ func main(){
 	store:=db.NewStore(conn)
 	server:=api.NewServer(store)
 
-	err=server.Start(serverAddress)
+	err=server.Start(config.ServerAddress)
 	if err!=nil{
 		log.Fatal("cannot start server",err)
 	}
