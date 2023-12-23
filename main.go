093b086e848dce292b32f25b4a9e3fcc54567504
Debayan Biswas <debayanbiswas1111@gmail.com>
2023-12-23 19:42:39 +0530

    Implemented rest apis using gin

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+	"github.com/pegasus7d/simplebank/api"
+	db "github.com/pegasus7d/simplebank/db/sqlc"
+)
+
+const (
+	dbDriver="postgres"
+	dbSource="postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress="0.0.0.0:8080"
+)
+
+
+
+func main(){
+	conn,err:=sql.Open(dbDriver,dbSource)
+	if err !=nil{
+		log.Fatal("Cannot connect to db:",err)
+	}
+
+
+	store:=db.NewStore(conn)
+	server:=api.NewServer(store)
+
+	err=server.Start(serverAddress)
+	if err!=nil{
+		log.Fatal("cannot start server",err)
+	}
+
+}
